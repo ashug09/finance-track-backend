6af00a1375ca45cb16aa748c5082835bb067bf1b
ashug09 <79792289+ashug09@users.noreply.github.com>
2024-03-13 23:47:09 +0530

    Day 10
    added log in, log out functionality in the application

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const registerUser = async (req, res) => {
   try {
     const body = req.body;
@@ -28,6 +30,108 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const body = req.body;
+    const user = await User.findOne({ email: body.email }).select(
+      "-refreshToken"
+    );
+    if (!user) return res.status(404).json({ message: `user not found in db` });
+    const isCorrectPassword = await bcrypt.compare(
+      body.password,
+      user.password
+    );
+    if (!isCorrectPassword)
+      return res.status(400).json({ message: `password is not correct` });
+    const finalUser = await User.findOne({ email: body.email }).select(
+      "-password -refreshToken"
+    );
+
+    const accessToken = generateAccessToken(user._id);
+    // const refreshToken = generateRefreshToken(user._id);
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json({
+        message: `user found: ${finalUser + accessToken}`,
+      });
+  } catch (error) {
+    res.status(400).json({
+      message: `some error occured at loginUser: ${error}`,
+    });
+  }
+};
+
+//generating access token is a part of logging in user
+const generateAccessToken = (userId) => {
+  try {
+    const accessToken = jwt.sign(
+      {
+        _id: userId,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+    return accessToken;
+  } catch (error) {
+    res.status(400).json({
+      message: `some error occured while generating access tokens ${error}`,
+    });
+  }
+};
+
+// const generateRefreshToken = async (userId) => {
+//   try {
+//     const refreshToken = jwt.sign(
+//       {
+//         _id: userId,
+//       },
+//       process.env.REFRESH_TOKEN_SECRET,
+//       {
+//         expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+//       }
+//     );
+//     const user = await User.findById({ _id: userId });
+//     user.refreshToken = refreshToken;
+//     user.save({ validateBeforeSave: false });
+//     return refreshToken;
+//   } catch (error) {
+//     res.status(400).json({
+//       message: `some error occured while generating refresh tokens ${error}`,
+//     });
+//   }
+// };
+
+const logOut = (req, res) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").replace("Bearer ", "");
+    //decoded token contains all the information that is passed into the token while signing it at the beginning
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken)
+      return res.status(400).json({ message: `unauthorized token` });
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    res.status(200).clearCookie("accessToken", token, options).json({
+      message: `logged out from website successfully`,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: `some error occured at ${error}`,
+    });
+  }
+};
 const userAllGroups = async (req, res) => {
   try {
     const body = req.body;
@@ -39,4 +143,4 @@ const userAllGroups = async (req, res) => {
     });
   }
 };
-export { registerUser, userAllGroups };
+export { registerUser, userAllGroups, loginUser, logOut };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,14 @@
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logOut,
+} from "../controllers/user.controller.js";
 import { userAllGroups } from "../controllers/user.controller.js";
 import { Router } from "express";
 const router = Router();
 const allgroups = Router();
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(logOut);
 allgroups.route("/allgroups").get(userAllGroups);
 export { router, allgroups };
